e832c41e34a7b85b1727a3bc93d308d366985185
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-04-12 18:30:39 -0700

    add react-bootstrap

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,12 @@
+import { Container, Navbar, Nav } from "react-bootstrap";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Navbar bg='dark' variant='dark'>
+      <Container>
+        <Navbar.Brand href='#home'>Culture</Navbar.Brand>/
+      </Container>
+    </Navbar>
+  );
 };
 export default Home;

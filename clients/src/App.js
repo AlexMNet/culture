6a1b3832985315ea4370060ec443e0a01695f23c
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-04-12 17:57:32 -0700

    add clients folder back

---
+++ clients/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Error } from "./pages";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
